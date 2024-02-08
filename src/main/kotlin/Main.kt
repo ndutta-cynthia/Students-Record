5690bd0c148a15b6eabfdc8adfaa63cd3ec97318
ndutta-cynthia <nduttamaina@gmail.com>
2024-02-08 21:26:06 +0300

    add file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+import java.security.cert.TrustAnchor
+
+fun main() {
+    var fullName = "Cynthia Maina"
+    var age = 23
+    var phoneNumber = "+254712345678"
+    var weight = 59.9F
+    var kenyan = true
+    println(fullName)
+    println(age)
+    println(phoneNumber)
+    println(weight)
+    println(kenyan)
+
+}
+
+
+
+
+
